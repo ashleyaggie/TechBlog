07041baa1abe813122d8e5d48ef963e177ac0a3d
Ashley <ashleyaggie@aol.com>
2021-05-16 19:03:51 -0500

    Minor changes.

--- public/js/addComment.js
+++ public/js/addComment.js
@@ -1,25 +1,26 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const contents = document.querySelector('#comment-text').value.trim();
-  
-    if (contents) {
-      const response = await fetch(`/api/comments`, {
-        method: 'POST',
-        body: JSON.stringify({ contents }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert('Failed to create comment');
-      }
+  event.preventDefault();
+
+  const contents = document.querySelector('#comment-text').value.trim();
+
+  if (contents) {
+    const post_id = req.params.id;
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ contents, post_id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Failed to create comment');
     }
-  };
-  
-  document
-    .querySelector('.new-comment-form')
-    .addEventListener('submit', newFormHandler);
+  }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newFormHandler);
